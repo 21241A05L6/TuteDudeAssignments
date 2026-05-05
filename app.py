953605902cb253f9778c37743927168973c09506
21241A05L6 <photospvn@gmail.com>
2026-05-05 14:29:01 +0000

    Updated to GenAI summarizer app

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
 import streamlit as st
+from transformers import pipeline
 
-st.set_page_config(page_title="GenAI Deployment App")
+st.set_page_config(page_title="AI Text Summarizer")
 
-st.title("🧠 GenAI Deployment Assignment")
+st.title("🧠 GenAI Text Summarizer")
 
-user_input = st.text_input("Enter your name")
+st.write("Enter a paragraph and generate AI summary using Transformers.")
 
-if st.button("Generate Greeting"):
-    st.success(f"Hello, {user_input}! Your GenAI app is deployed successfully.")
+@st.cache_resource
+def load_model():
+    return pipeline("summarization", model="facebook/bart-large-cnn")
+
+summarizer = load_model()
+
+user_input = st.text_area("Enter Text")
+
+if st.button("Generate Summary"):
+
+    if user_input.strip() == "":
+        st.warning("Please enter some text.")
+    else:
+        try:
+            summary = summarizer(
+                user_input,
+                max_length=80,
+                min_length=20,
+                do_sample=False
+            )
+
+            st.success("Summary Generated Successfully!")
+
+            st.write(summary[0]['summary_text'])
+
+        except Exception as e:
+            st.error(f"Error: {e}")
